7f0ede8df1ae07f8d02c28c2ba97f8bab67cb432
Wilian Simões <wilian.simoes.alexandre@gmail.com>
2021-09-24 18:26:22 -0300

    Add HomeController.

---
+++ FreeGamesAPI/FreeGamesAPI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreeGamesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public ActionResult Index()
+        {
+            return Ok("ok");
+        }
+    }
+}
