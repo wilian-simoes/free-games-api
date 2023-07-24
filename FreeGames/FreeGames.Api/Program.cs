caa1a0f447e94226224441b9125d91218ef334b4
Wilian Simões <wilian.simoes.alexandre@gmail.com>
2023-07-24 17:10:25 -0300

    publish in aws lambda.

--- FreeGames/FreeGames.Api/Program.cs
+++ FreeGames/FreeGames.Api/Program.cs
@@ -10,10 +10,13 @@ builder.Services.AddSwagger();
 builder.Services.AddAuthentication(builder.Configuration);
 builder.Services.RegisterServices(builder.Configuration);
 
+// Add AWS Lambda support.
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment .IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -21,15 +24,24 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseCors(builder => builder
-            .SetIsOriginAllowed(orign => true)
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials());
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+    });
+});
 
-app.MapControllers();
+//app.UseCors(builder => builder
+//            .SetIsOriginAllowed(orign => true)
+//            .AllowAnyMethod()
+//            .AllowAnyHeader()
+//            .AllowCredentials());
 
 app.Run();
